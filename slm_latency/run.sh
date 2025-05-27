fc835b8bd57c7db309bf6f42d64f54f5709e44ff
Ubuntu <ubuntu@ip-10-0-163-96.us-west-2.compute.internal>
2025-05-27 02:21:56 +0000

    add ptx and sass

--- slm_latency/run.sh
+++ slm_latency/run.sh
@@ -1,10 +1,10 @@
 rm -r *.o *.ptx *.sass
-nvcc -arch=sm_100a -Xptxas -O3 -o slm_test.o slm_test.cu
-cuobjdump --dump-ptx slm_test.o > slm_test.ptx
-cuobjdump --dump-sass slm_test.o > slm_test.sass
+nvcc -arch=sm_100a -Xptxas -O3 -o main.o main.cu
+cuobjdump --dump-ptx main.o > main.ptx
+cuobjdump --dump-sass main.o > main.sass
 
 for device in {0..7}
 do
-    echo "Running slm_test.o on CUDA device $device"
-    CUDA_VISIBLE_DEVICES=$device ./slm_test.o
+    echo "Running main.o on CUDA device $device"
+    CUDA_VISIBLE_DEVICES=$device ./main.o
 done
