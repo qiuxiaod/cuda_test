a0ac42dea7763e090a9cdf8b7245235eb93c21c7
Ubuntu <ubuntu@ip-10-0-163-96.us-west-2.compute.internal>
2025-05-28 03:38:18 +0000

    add st+ld case

--- tmem_latency/main.cu
+++ tmem_latency/main.cu
@@ -10,6 +10,12 @@
 #define REP 8
 #endif
 
+// TEST_MODE == 0 means load only
+// TEST_MODE == 1 means store + load serialized
+#ifndef TEST_MODE
+#define TEST_MODE 0
+#endif
+
 // CUDA kernel to perform memory loads from shared memory and measure latency
 __global__ void benchmarkTMEMLoadLatency(unsigned long long *d_start, unsigned long long *d_end, uint32_t* data) {
     // Declare shared memory
@@ -35,18 +41,38 @@ __global__ void benchmarkTMEMLoadLatency(unsigned long long *d_start, unsigned l
     fence_view_async_tmem_store();
 
     __syncthreads();
-    unsigned long long start, end;
-
+    unsigned long long start[2];
+    unsigned long long end[2];
     uint32_t val_array_tmp[REP];
-    __syncwarp();
-    start = clock64();
 
-    tmem_ld_32dp32bNx<REP>(tmem_ptr, val_array_tmp);
-    fence_view_async_tmem_load();
+#if TEST_MODE == 0
+        __syncthreads();
+        __syncwarp();
+        start[0] = clock64();
 
-    val_array[0] += val_array_tmp[0];
+        tmem_ld_32dp32bNx<REP>(tmem_ptr, val_array_tmp);
+        fence_view_async_tmem_load();
 
-    end = clock64();
+        val_array[0] += val_array_tmp[0];
+
+        end[0] = clock64();
+#elif TEST_MODE == 1
+        __syncthreads();
+        __syncwarp();
+        start[0] = clock64();
+
+        tmem_st_32dp32bNx<REP>(tmem_ptr, val_array);
+        fence_view_async_tmem_store();
+        tmem_ld_32dp32bNx<REP>(tmem_ptr, val_array_tmp);
+        fence_view_async_tmem_load();
+
+        val_array[0] += val_array_tmp[0];
+        end[0] = clock64();
+#endif
+    // if(warp_id < 4){
+
+    // }
+
 
     __syncthreads();
 
@@ -57,8 +83,8 @@ __global__ void benchmarkTMEMLoadLatency(unsigned long long *d_start, unsigned l
 
     // Write the start and end times to global memory
     if (tid == 0) {
-        d_start[0] = start;
-        d_end[0] = end;
+        d_start[0] = start[0];
+        d_end[0] = end[0];
     }
 
     #pragma unroll
@@ -72,26 +98,34 @@ int main() {
     unsigned long long *d_start, *d_end;
     uint32_t *d_data;
     cudaMalloc(&d_data, sizeof(uint32_t) * THD_NUM * REP);
-    cudaMalloc(&d_start, sizeof(unsigned long long));
-    cudaMalloc(&d_end, sizeof(unsigned long long));
+    cudaMalloc(&d_start, sizeof(unsigned long long) * 2);
+    cudaMalloc(&d_end, sizeof(unsigned long long) * 2);
 
     // Launch the kernel
     benchmarkTMEMLoadLatency<<<1, THD_NUM>>>(d_start, d_end, d_data);
 
     // Copy the start and end times back to the host
-    unsigned long long h_start, h_end;
-    cudaMemcpy(&h_start, d_start, sizeof(unsigned long long), cudaMemcpyDeviceToHost);
-    cudaMemcpy(&h_end, d_end, sizeof(unsigned long long), cudaMemcpyDeviceToHost);
+    unsigned long long* h_start = (unsigned long long*)malloc(sizeof(unsigned long long) * 2);
+    unsigned long long* h_end = (unsigned long long*)malloc(sizeof(unsigned long long) * 2);
 
-    // Calculate the latency in clock cycles
-    double latency = (h_end - h_start);
+    cudaMemcpy(h_start, d_start, sizeof(unsigned long long) * 2, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_end, d_end, sizeof(unsigned long long) * 2, cudaMemcpyDeviceToHost);
 
-    // Print the result
-    std::cout << "TMEM Load Latency: " << latency << " clock cycles" << std::endl;
-
+    #if TEST_MODE == 0
+        // Calculate the latency in clock cycles
+        double latency = (h_end[0] - h_start[0]);
+        // Print the result
+        std::cout << "TMEM Load Latency: " << latency << " clock cycles" << std::endl;
+    #elif TEST_MODE == 1
+        double latency = (h_end[0] - h_start[0]);
+        std::cout << "TMEM Store + Load Latency: " << latency << " clock cycles" << std::endl;
+    #endif
     // Clean up
     cudaFree(d_start);
     cudaFree(d_end);
+    free(h_start);
+    free(h_end);
+    cudaFree(d_data);
 
     return 0;
 }
--- tmem_latency/run.sh
+++ tmem_latency/run.sh
@@ -1,6 +1,6 @@
 rm -r *.o *.ptx *.sass
 
-rep_list=(1 2 4 8 16 32 64 128)
+rep_list=(1 2 4 8 16 32 64)
 
 # Loop over each value of rep
 for rep in "${rep_list[@]}"
@@ -9,14 +9,26 @@ do
     echo "============================================================================="
     echo "============== Test tcgen05.ld.sync.aligned.32x32b.x${rep} ====================="
     echo "============================================================================="
-    nvcc -arch=sm_100a -Xptxas -O3 -DREP=$rep -o main_$rep.o main.cu
-    cuobjdump --dump-ptx main_$rep.o > main_$rep.ptx
-    cuobjdump --dump-sass main_$rep.o > main_$rep.sass
+    nvcc -arch=sm_100a -Xptxas -O3 -DREP=$rep -DTEST_MODE=0 -o main_mode0_$rep.o main.cu
+    cuobjdump --dump-ptx main_mode0_$rep.o > main_mode0_$rep.ptx
+    cuobjdump --dump-sass main_mode0_$rep.o > main_mode0_$rep.sass
     for itr in {0..7}
     do
-        # echo "Running main_$rep.o on CUDA Itr$itr"
-        CUDA_VISIBLE_DEVICES=4 ./main_$rep.o
+        CUDA_VISIBLE_DEVICES=3 ./main_mode0_$rep.o
     done
 done
 
-
+for rep in "${rep_list[@]}"
+do
+    # Compile the CUDA program with the current value of rep
+    echo "============================================================================="
+    echo "============= Test tcgen05.st+ld.sync.aligned.32x32b.x${rep} ===================="
+    echo "============================================================================="
+    nvcc -arch=sm_100a -Xptxas -O3 -DREP=$rep -DTEST_MODE=1 -o main_mode1_$rep.o main.cu
+    cuobjdump --dump-ptx main_mode1_$rep.o > main_mode1_$rep.ptx
+    cuobjdump --dump-sass main_mode1_$rep.o > main_mode1_$rep.sass
+    for itr in {0..7}
+    do
+        CUDA_VISIBLE_DEVICES=3 ./main_mode1_$rep.o
+    done
+done
